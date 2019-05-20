340871a9fa2a763d1b842015f9b163d59b5ca62e
ritaly <malgorzata.lyczywek@gmail.com>
2019-05-20 20:19:46 +0200

    moj folder 1

---
+++ sec1/bmi.py
@@ -0,0 +1,4 @@
+h = float(input('Twoj wzrost (m): '))
+w = float(input('Twoja waga (kg): '))
+bmi = w / (h ** 2)
+print("Your BMI is:", round(bmi, 2))
---
+++ sec1/weeks.py
@@ -0,0 +1,6 @@
+# Napisz program, który obliczy i wyświetli liczbę minut w ciągu siedmiu tygodni.
+hour = 60
+day = 24 * hour
+week = 7 * day
+
+print('7 weeks = ', 7 * week, 'minutes')
