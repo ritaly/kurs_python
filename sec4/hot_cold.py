1881644dc8658e1fae4ba119c4333aa5a2670fcb
ritaly <malgorzata.lyczywek@gmail.com>
2019-05-21 12:00:49 +0200

    podpowiedzi do zadania hot-cold

--- sec4/hot_cold.py
+++ sec4/hot_cold.py
@@ -6,3 +6,17 @@
 # Jeśli użytkownik zgadnie wygrywa gracz.
 # Jeśli po 6 próbach użytkownik nie zgadnie - wygrywa komputer.
 
+# zaimportuj bibliotekę
+
+# zmienne:
+# - licznik szans
+# - bliskosc trafienia
+
+# pętla do 6 powtórzeń
+# - pobranie danych od uzytkownika
+# - sprawdzenie warunku czy uzytkownik zgadł
+# - sprawdzenie warunku czy to już 6 powtorzenie
+# - "else"
+# - - czy nowe trafienie jest blizesz niz poprzednie (wartosc bezwzgledna)? - zaktualizuj bliskosc trafienia - wyswietl cieplej
+# - - czy nowe trafienie jest dalsze? - wyswietl Zimniej
+# - - else : może wyświetlic informacje ze ani zimniej ani cieplej
