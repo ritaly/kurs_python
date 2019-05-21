a13994e75e1112fc5217a13decdf233397f3e3a4
ritaly <malgorzata.lyczywek@gmail.com>
2019-05-21 12:11:21 +0200

    solutions 4

---
+++ done/sec3_done/zad3.py
@@ -0,0 +1,8 @@
+# Zadanie 3
+# Dla podanej liczby w systemie dwójkowym bin_num = 1001111
+# oblicz wartość w systemie dziesiętnym.
+bin_num = 1001111
+dec_num = (1*(2**6)) + (0*(2**5)) + (0*(2**4)) + (1*(2**3)) + \
+          (1*(2**2)) + (1*(2**1)) + (1*(2**0))
+
+print(bin_num, 'w systemie dziesiętnym to:', dec_num)
---
+++ done/sec3_done/zad5.py
@@ -0,0 +1,7 @@
+# Zadanie 5
+# Zwróć wartość True, jeżeli liczba jest parzysta.
+num = int(input("Podaj liczbe calkowita: "))
+
+is_even = bool(not(num % 2))
+
+print("Is the number even? ", is_even)
