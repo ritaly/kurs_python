8d67a1abbda199ae1ab05bc1b80924490f9d878e
ritaly <malgorzata.lyczywek@gmail.com>
2019-06-26 13:15:55 +0200

    solution

---
+++ 16_unittest/test_student-1.py
@@ -0,0 +1,25 @@
+from unittest import TestCase
+from student import Student
+
+
+class TestStudent(TestCase):
+
+    def test_email(self):
+        obj_anna = Student('ana', 'kowalska', 4.6, True)
+
+        self.assertEqual(obj_anna.email, 'ana.kowalska@university.com')
+
+        obj_anna.name = 'anna'
+        self.assertEqual(obj_anna.email, 'anna.kowalska@university.com')
+
+    def test_fullname(self):
+        obj_anna = Student('anna', 'kowalska', 4.6, True)
+
+        self.assertEqual(obj_anna.fullname, 'Anna Kowalska')
+
+        obj_anna.last = 'Zamezna'
+        self.assertEqual(obj_anna.fullname, 'Anna Zamezna')
+
+
+if __name__ == '__main__':
+    unittest.main()
